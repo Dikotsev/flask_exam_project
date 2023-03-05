2d57f1eea804a60c59f52c25b9d71803afca668d
Dilyan <dikotsev@gmail.com>
2023-03-05 20:58:54 +0000

    changed number of characters for different class arguments

--- models/user.py
+++ models/user.py
@@ -9,10 +9,10 @@ class BaseUserModel(db.Model):
     __abstract__ = True
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    username = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False, unique=True)
+    name = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(250), unique=True)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(250), nullable=False, unique=True)
     phone = db.Column(db.String(15), nullable=False)
 
 
