3ecd646c50736d517988d7ed695731bd2390a4a5
Dilyan <dikotsev@gmail.com>
2023-03-05 20:55:47 +0000

    create managers package and users.py file. in users.py file all business logic of seller register, seller login and user register and user login

--- managers/users.py
+++ managers/users.py
@@ -3,10 +3,11 @@ from passlib.hash import sha256_crypt
 from werkzeug.exceptions import BadRequest
 from models.auth import AuthManager
 from models.seller import SellerModel
+from models.user import UserModel
 from db import db
 
 
-class SellerManager():
+class SellerManager:
 
 
     @staticmethod
@@ -15,7 +16,7 @@ class SellerManager():
         seller = SellerModel(**seller_data)
         try:
             db.session.add(seller)
-            db.session.flush()
+            db.session.commit()
             return AuthManager.encode_token(seller)
         except Exception as ex:
             raise BadRequest(str(ex))
@@ -24,9 +25,35 @@ class SellerManager():
     @staticmethod
     def login(data):
         try:
-            seller = SellerModel.querry.filter_by(email = data["email"]).first()
+            seller = SellerModel.query.filter_by(email=data["email"]).first()
             if seller and check_password_hash(seller.password, data["password"]):
                return AuthManager.encode_token(seller)
-            raise Exeption
+            raise Exception
+        except Exception:
+            raise BadRequest("Invalid email or password")
+
+
+class UserManager:
+
+
+    @staticmethod
+    def register(user_data):
+        user_data["password"] = generate_password_hash(user_data["password"], method="sha256")
+        user = UserModel(**user_data)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return AuthManager.encode_token(user)
+        except Exception as ex:
+            raise BadRequest(str(ex))
+
+
+    @staticmethod
+    def login(data):
+        try:
+            user = UserModel.query.filter_by(email=data["email"]).first()
+            if user and check_password_hash(user.password, data["password"]):
+               return AuthManager.encode_token(user)
+            raise Exception
         except Exception:
             raise BadRequest("Invalid email or password")
