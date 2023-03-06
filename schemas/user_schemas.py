849a541c83bd2c6451aedd5d7d524d69bd6b7af3
Dilyan <dikotsev@gmail.com>
2023-03-06 22:07:24 +0000

    add schemas changes

---
+++ schemas/user_schemas.py
@@ -0,0 +1,12 @@
+from marshmallow import Schema, fields
+
+class UserSchema(Schema):
+    email = fields.Email(required=True)
+    password = fields.String(required=True)
+
+class UserRegisterSchema(UserSchema):
+    name = fields.String(min_length=2, required=True)
+    phone =fields.String(max_length=12, required=True)
+
+class UserLoginSchema(UserSchema):
+    pass
