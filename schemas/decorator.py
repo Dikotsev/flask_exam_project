df78a20fba54a9e35b201f57170b7a1e5feba3c4
Dilyan <dikotsev@gmail.com>
2023-03-26 21:57:49 +0100

    Changes in schemas - decorator.py and user_schemas

--- schemas/decorator.py
+++ schemas/decorator.py
@@ -1,16 +1,31 @@
 from functools import wraps
 from flask import request
-from werkzeug.exceptions import BadRequest
+from werkzeug.exceptions import BadRequest, Forbidden
+from models.auth import auth
 
 
 def validate_schema(schema_name):
-    def decorator(f):
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
+    def decorated_function(f):
+        def wrapper(*args,**kwargs):
+            data = request.get_json()
             schema = schema_name()
-            errors = schema.validate(request.get_json())
-            if errors:
-                raise BadRequest (f"Invalid fields{errors}")
+            errors = schema.validate(data)
+            if not errors:
+                return f(*args, **kwargs)
+            raise BadRequest (f"Invalid fields{errors}")
+        return wrapper
+    return decorated_function
+
+
+
+def permission_required(role):
+    def decorated_function(f):
+        def wrapper(*args, **kwargs):
+            current_user = auth.current_user()
+            if not current_user.role == role:
+                raise Forbidden("Permission denied!")
             return f(*args, **kwargs)
-        return decorated_function
-    return decorator
+        return wrapper
+    return decorated_function
+
+
--- schemas/user_schemas.py
+++ schemas/user_schemas.py
@@ -1,12 +1,16 @@
 from marshmallow import Schema, fields
 
+
 class UserSchema(Schema):
     email = fields.Email(required=True)
     password = fields.String(required=True)
 
+
 class UserRegisterSchema(UserSchema):
     name = fields.String(min_length=2, required=True)
-    phone =fields.String(max_length=12, required=True)
+    phone = fields.String(max_length=12, required=True)
+
 
 class UserLoginSchema(UserSchema):
-    pass
+#  email = fields.Email(load_from="email" ,dump_to="email")
+   pass
